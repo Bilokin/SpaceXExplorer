71908b1b1a0fa70a6dd7177840efc54a97bdabbc
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2026-01-10 19:45:02 +0100

    Units

--- tests/test_info_fetcher.py
+++ tests/test_info_fetcher.py
@@ -1,13 +1,13 @@
 from spacexexplorer.info_manager import InfoManager
 import os
-
+import json
 
 def test_fetch_static():
     info = InfoManager()
     info.fetch_static()
     files = os.listdir('.')
     print(files)
-    assert len(files) == 4
+    assert len(files) == len(info.static_file_dict)
 
 
 def test_get():
@@ -19,3 +19,26 @@ def test_get():
     assert company is not None
     assert 'headquarters' in company
     assert 'links' in company
+
+
+def test_filter_launches():
+    info_manager = InfoManager()
+    test_objs = [{"success": True},
+                     {"success": True},
+                     {"success": False},
+                     {"success": False},
+                     {"success": True},
+    ]
+    with open( f'launches.json', 'w') as f:
+        json.dump(test_objs, f, indent='    ')
+
+    objs = info_manager.get("launches")
+    print(objs)
+    assert len(objs) == 5
+
+    all_objs = info_manager.filter_launches(**{})
+    assert len(objs) == len(all_objs)
+    success_objs = info_manager.filter_launches(**{"success": True})
+    assert len(success_objs) == 3
+    fail_objs = info_manager.filter_launches(**{"success": False})
+    assert len(fail_objs) == 2
